a16e96cff1fab592b33e791e8a21b0e35bb5f11f
Arnaud80 <arnaud@duhamel.biz>
2023-12-24 07:11:59 +0100

    ?

--- src/index.js
+++ src/index.js
@@ -3,13 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import '@vtmn/icons/dist/vitamix/font/vitamix.css';
 import App from './App.js'
-import { GoogleOAuthProvider } from '@react-oauth/google';
 
 // ========================================
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <GoogleOAuthProvider clientId="952251694198-0bimgqri5267a9gm14a1e5pmmnuvr634.apps.googleusercontent.com">
         <App className="block"/>
-    </GoogleOAuthProvider>
 );
